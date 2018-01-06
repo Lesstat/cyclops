4ff7568271c42deaca70fa76e3f34d50f3a6d3cc
Florian Barth <florianbarth@gmx.de>
2018-01-06 15:14:13 +0100

    Improve output during contraction

--- src/cr_lib/contractor.cpp
+++ src/cr_lib/contractor.cpp
@@ -164,8 +164,6 @@ Graph Contractor::contract(Graph& g)
   auto set = reduce(independentSet(g), g);
   std::vector<Node> nodes{};
   std::vector<Edge> edges{};
-  size_t contracted = 0;
-  const size_t tenPercent = (set.size() > 10 ? set.size() : 10) / 10;
 
   for (size_t i = 0; i < g.getNodeCount(); ++i) {
     NodePos pos{ i };
@@ -177,10 +175,6 @@ Graph Contractor::contract(Graph& g)
         }
       }
     } else {
-      if (contracted % tenPercent == 0) {
-        std::cout << "contracting " << contracted << "th node" << '\r';
-      }
-      contracted++;
       auto newShortcuts = contract(g, pos);
       std::move(newShortcuts.begin(), newShortcuts.end(), std::back_inserter(shortcuts));
 
@@ -223,11 +217,14 @@ Graph Contractor::contractCompletely(Graph& g)
 
   Graph intermedG = contract(g);
   int uncontractedNodesPercent = intermedG.getNodeCount() * 100 / g.getNodeCount();
-  std::cout << 100 - uncontractedNodesPercent << "% of the graph is contracted" << '\n';
+  std::cout << 100 - uncontractedNodesPercent << "% of the graph is contracted" << '\r'
+            << std::flush;
   while (uncontractedNodesPercent > 10) {
     intermedG = contract(intermedG);
     uncontractedNodesPercent = intermedG.getNodeCount() * 100 / g.getNodeCount();
-    std::cout << 100 - uncontractedNodesPercent << "% of the graph is contracted" << '\n';
+    std::cout << 100 - uncontractedNodesPercent << "% of the graph is contracted" << '\r'
+              << std::flush;
   }
+  std::cout << '\n';
   return mergeWithContracted(intermedG);
 }
